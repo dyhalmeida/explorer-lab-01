a42ae0aa9553017728c8db499f940f22f571ccc2
Diego Almeida <diego.almeida@memed.com.br>
2022-10-18 09:19:16 -0300

    feat(explorer-lab-01): add setCardType

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
 import "./css/index.css"
+
+const creditCardBgColorPrimary = document.querySelector(
+  ".cc-bg svg > g g:nth-child(1) path"
+)
+const creditCardBgColorSecondary = document.querySelector(
+  ".cc-bg svg > g g:nth-child(1) path"
+)
+const creditCardLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    default: ["#000000", "#CCCCCC"],
+  }
+
+  const [primary, secondary] = colors[type]
+  const logoSvg = `cc-${type}.svg`
+
+  creditCardBgColorPrimary.setAttribute("fill", primary)
+  creditCardBgColorSecondary.setAttribute("fill", secondary)
+  creditCardLogo.setAttribute("src", logoSvg)
+}
+
+globalThis.setCardType = setCardType
